beed19f89885e0302e572da4703dff5404068ac3
maik <maik.masl@gmail.com>
2025-09-05 20:58:54 -0300

    adicionada verificação de números primos

--- modules/dh.py
+++ modules/dh.py
@@ -1,8 +1,12 @@
 from random import randint
+from modules.primo import verificar_primo
 
 class DiffieHellman():
     
     def __init__(self, G, N):
+
+        if not (verificar_primo(G) & verificar_primo(N)):
+            raise Exception("Números do DiffieHellman não são primos!")
         
         self.meuvalor = randint(1, 99999)
         self.G = G
